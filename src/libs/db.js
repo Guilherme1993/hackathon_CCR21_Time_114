860ccbf05f257dd4462d36892a2128893a627dfb
Guilherme Nascimento <guinasc7@gmail.com>
2021-01-17 18:22:59 -0300

    feat: Validação e efetuação de cadastro, login e recuperação

--- src/libs/db.js
+++
@@ -1,5 +0,0 @@
-var db = openDatabase('dbProPose', '1.0', 'dbProPoseCCR2021', 2 * 1024 * 1024);
-
-db.transaction(tx => {
-    tx.executeSql('CREATE TABLE users (ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, mail TEXT, password TEXT, type INTEGER, access INTEGER)');
- });
--- src/libs/login.js
+++ src/libs/login.js
@@ -1,25 +1,60 @@
-const formLogin = document.querySelector('.login') 
+const formLogin = document.querySelector('.login')
+const inputEmail = formLogin.querySelector('.email');
+const inputSenha = formLogin.querySelector('.senha');
 
-// const arrayUsuarios = localStorage.getItem('usuarios')
-const arrayUsuarios = []
+const arrayUsuarios = JSON.parse(localStorage.getItem('usuarios'))
 
-const usuario = arrayUsuarios.filter(usuario => {
-  usuario.email === inputEmail.value
-})
+function acessar() {
 
-if (usuario.length != 0) {
-  if (usuario.senha === inputSenha.value) {
-    const redirecionamento = {
-      '0': window.location.href = 'http://127.0.0.1:5500/candidato/index.html',
-      '1': window.location.href = 'http://127.0.0.1:5500/profissional/index.html',
-      '2': window.location.href = 'http://127.0.0.1:5500/admin/index.html',
+  if (validar()) {
+
+    const usuario = arrayUsuarios.filter(us => {
+      return us.email === inputEmail.value;
+    })
+
+    if (usuario.length != 0) {
+      if (usuario[0].senha === inputSenha.value) {
+        localStorage.setItem('usuarioLogado', JSON.stringify(usuario[0]));
+        const redirecionamento = {
+          // 0: window.location.href = 'http://127.0.0.1:5500/candidato/index.html',
+          // 1: window.location.href = 'http://127.0.0.1:5500/profissional/index.html',
+          // 2: window.location.href = 'http://127.0.0.1:5500/admin/index.html',
+          0: window.location.href = 'file:///C:/Users/guisk/Documents/ADS%20-%20Fatec/hackathon_ccr/hackathon_CCR21_Time_114/hackathon_CCR21_Time_114/candidato/index.html',
+          1: window.location.href = 'file:///C:/Users/guisk/Documents/ADS%20-%20Fatec/hackathon_ccr/hackathon_CCR21_Time_114/hackathon_CCR21_Time_114/profissional/index.html',
+          2: window.location.href = 'file:///C:/Users/guisk/Documents/ADS%20-%20Fatec/hackathon_ccr/hackathon_CCR21_Time_114/hackathon_CCR21_Time_114/admin/index.html',
+        }
+        redirecionamento[usuario[0].tipo]
+
+      } else {
+        alert('Senha inválida!')
+        return false;
+      }
+    } else {
+      alert('Usuário não cadastrado!')
+      return false;
     }
-    redirecionamento[usuario.tipo]
-  }
-}
-console.log(usuario)
 
-formLogin.addEventListener('submit', e => {
-  // window.location.href = 'http://127.0.0.1:5500/src/pages/aluno/index.html'
-  e.preventDefault()
-})
+    return true;
+
+  } else {
+    return false;
+  }
+
+  function validar() {
+
+    let valido = true;
+
+    if (inputEmail.value.trim() == '') {
+      alert('Digite o e-mail');
+      inputEmail.focus();
+      valido = false;
+    } else if (inputSenha.value.trim() == '') {
+      alert('Digite a senha');
+      inputSenha.focus();
+      valido = false;
+    }
+
+    return valido;
+  }
+
+}
